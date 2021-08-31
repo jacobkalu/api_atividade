8b5a1ecaa0f4f855ea84fb72b8e086b8acd1bc05
jacobkalu <conta.jacobfranco@gmail.com>
2021-08-30 21:04:49 -0300

    Incluindo app flask para disponibilizar REST APIs para manipulação das tabelas de pessoas e atividades, também incluído o requirements do projeto

---
+++ app.py
@@ -0,0 +1,110 @@
+# Importando as libs necessárias
+from flask import Flask, request
+from flask_restful import Resource, Api
+from models import Pessoas, Atividades
+
+# nomeando os apps
+app = Flask(__name__)
+api = Api(app)
+
+
+# Recuperando os dados por nome de pesssoas e inserindo num dict (response)
+class Pessoa(Resource):
+
+    #Definindo o método de recuperação GET
+    def get(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        try:
+            response = {
+                'nome': pessoa.nome,
+                'idade': pessoa.idade,
+                'id': pessoa.id
+            }
+        #Tratando erros de atributo ( quando o nome não conta no bd)
+        except AttributeError:
+            response = {'status': 'erro', 'mensagem': 'Pessoa fora dos registros'}
+
+        return response
+
+    #Definindo o método de alteração PUT
+    def put(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        dados = request.json
+        if 'nome' in dados:
+            pessoa.nome = dados['nome']
+
+        if 'idade' in dados:
+            pessoa.idade = dados['idade']
+
+        #Adicionando os dados no bd
+        pessoa.save()
+        response = {
+            'id': pessoa.id,
+            'nome': pessoa.nome,
+            'idade': pessoa.idade
+        }
+        return response
+
+    #Definindo o método de exclusão DELETE
+    def delete(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        mensagem = 'Pessoa {} excluída com sucesso'.format(pessoa.nome)
+
+        #Excluindo a pesson no bd pela chamandoa função delete()
+        pessoa.delete()
+        return {'status': 'Sucesso!', 'mensagem':mensagem}
+
+
+# Definindo o método de listar todas as pessoas no bd
+class ListaPessoas(Resource):
+
+    # Definindo o método de recuperação GET
+    def get(self):
+        pessoas = Pessoas.query.all()
+        response = [{'id': i.id, 'nome': i.nome, 'idade': i.idade} for i in pessoas]
+        return response
+
+    # Definindo o método de inserção POST
+    def post(self):
+        dados = request.json
+        pessoa = Pessoas(nome=dados['nome'], idade=dados['idade'])
+
+        #Salvando o novo registro no bd com o método pessoa.save definida em util.py
+        pessoa.save()
+        response = {
+            'id': pessoa.id,
+            'nome': pessoa.nome,
+            'idade': pessoa.idade
+        }
+        return response
+
+# Definindo os verbos GET, POST, PUT e DELETE pata Atividades
+class ListaAtividades(Resource):
+    # Recuperando as atividade pelo GET e listando-as
+    def get(self):
+        atividades = Atividades.query.all()
+        response = [{'id':i.id, 'nome': i.nome, 'pessoa': i.pessoa.nome } for i in atividades]
+        return response
+
+    def post(self):
+        dados = request.json
+        pessoa = Pessoas.query.filter_by(nome=dados['pessoa']).first()
+        atividade = Atividades(nome=dados['nome'], pessoa=pessoa)
+
+        # Salvando o novo registro no bd com o método atividade.save definida em util.py
+        atividade.save()
+        response = {
+            'pessoa' : atividade.pessoa.nome,
+            'nome' : atividade.nome,
+            'id': atividade.id,
+        }
+        return response
+
+
+# Registrando os métodos
+api.add_resource(Pessoa, '/pessoa/<string:nome>/')
+api.add_resource(ListaPessoas, '/pessoa/')
+api.add_resource(ListaAtividades, '/atividades/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
